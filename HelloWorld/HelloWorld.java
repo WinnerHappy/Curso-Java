f78f597234365822435a9f199b987c2a0eda2ddb
Winner Happy <45710487+WinnerHappy@users.noreply.github.com>
2024-03-16 10:48:22 -0300

    Hello World

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,10 @@
+/**
+ * The class HelloWorld contains a main method that prints "Hello World" and includes a TODO comment.
+ */
+
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+    //TODO:HelloWorld
 }
